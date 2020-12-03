4c6edacbe7193999b320b184611830ea26fd379b
kate <kate.bu33@gmail.com>
2020-12-03 15:54:13 -0600

    thining

---
+++ R/thining.R
@@ -0,0 +1,27 @@
+#' thining function
+#'
+#' @param sris
+#' @param thinby
+#'
+#' @return
+#' @export
+#'
+#' @examples
+thining <- function(sris,thinby=1) {
+
+  if (is.vector(sris)) {
+
+    sris.len <- length(sris)
+    tmp <- seq(1,sris.len,by=thinby)
+    sris.thin <- sris[tmp]
+
+  } else  if (is.matrix(sris)) {
+
+    sris.len <- dim(sris)[1]
+    tmp <- seq(1,sris.len,by=thinby)
+    sris.thin <- as.matrix(sris[tmp,])
+  }
+
+  return(list(sris.thin))
+}
+
