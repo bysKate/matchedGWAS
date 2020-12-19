75c7aa414b9d39e5326a2f34c06c371b37ddceb8
kate <kate.bu33@gmail.com>
2020-12-19 16:59:10 -0600

    Add swap options in mcmc_posterior.R.

--- R/mcmc_posterior.R
+++ R/mcmc_posterior.R
@@ -2,6 +2,8 @@
 #'
 #' @param cur_gid
 #' @param saveResult
+#' @param swap
+#' @param swap_cutoff
 #'
 #' @return
 #' @export
@@ -9,11 +11,26 @@
 #' @examples
 mcmc_posterior <- function(
   cur_gid,
+  swap = T,
+  swap_cutoff = 0.8,
   saveResult = T
 ) {
 
   #  load  dataset
-  ssample <<- as.matrix(allsnp_list %>% filter(gseg_id==cur_gid) %>% select(X00:X22), ncol = 9)
+  ssample <<- as.matrix(
+    allsnp_list %>% filter(gseg_id==cur_gid) %>% select(X00:X22),
+    ncol = 9
+  )
+
+  # reverse risk allele
+  if (isTRUE(swap)) {
+    rec_idx <- (allsnp_list$rr_mle[allsnp_list$gseg_id==cur_gid] < swap_cutoff)
+    ssample[rec_idx,] <<- t(
+      apply(
+        matrix(ssample[rec_idx,], byrow = T, ncol = 9), 1, rev
+      )
+    )
+  }
 
   gmdl <<- 'add0'
   snp.size <<- dim(ssample)[1]
@@ -25,15 +42,15 @@ mcmc_posterior <- function(
 
 
   # mcmc 1
-  update_mcmc(MCchains_len,initl = 1)
+  update_mcmc(MCchains_len, use_seed = 1)
   mclist1 <- esti.list; rm(esti.list, envir = .GlobalEnv)
 
   # mcmc 2
-  update_mcmc(MCchains_len,initl = 2)
+  update_mcmc(MCchains_len, use_seed = 2)
   mclist2 <- esti.list; rm(esti.list, envir = .GlobalEnv)
 
   # mcmc 3
-  update_mcmc(MCchains_len,initl = 3)
+  update_mcmc(MCchains_len, use_seed = 3)
   mclist3 <- esti.list; rm(esti.list, envir = .GlobalEnv)
 
   # combine results to list
